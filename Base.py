decdc67cfc3e131e42841f260f5ba1c102591370
o-ordonez <omarordonezlima@Air-de-Omar.fritz.box>
2020-06-08 14:34:29 +0200

    Azucar añadido

--- Base.py
+++ Base.py
@@ -2,4 +2,5 @@ Productos = {
     "CACAHUATE": [1, "Cacahuate", "GR", 10],
     "AMARANTO": [2, "Amaranto", "GR", 15],
     "PEPITA": [3, "Pepita", "GR", 17],
+    "AZUCAR": [4, "Azucar", "GR", 17],
 }
