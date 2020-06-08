7a52fb3e245c1260d758060dd4b9b26d2163d69c
o-ordonez <omarordonezlima@Air-de-Omar.fritz.box>
2020-06-08 20:37:04 +0200

    Caja lista

--- Caja.py
+++ Caja.py
@@ -1,15 +1,29 @@
 from Base import Productos
 
-Articulo = input("Que articulo desea registrar: ")
+# variables que se van a usar
+Total = 0
+Venta_tot = []
+Articulo = str(input("Que articulo desea registrar: ").upper())
+
+# loop para introducir todos los articulos que se van a vender
 while Articulo != "PAGAR":
     try:
         PUnitario = Productos[Articulo][3]
-        Cantidad = 10
+        Cantidad = int(input("Cuantos articulos: "))
         Venta = Productos[Articulo]
-        Venta.append(PUnitario * Cantidad)
-        print(Venta)
-        Articulo = "PAGAR"
+        Sub_total = PUnitario * Cantidad
+        Venta.append(Sub_total)
+        Total += Sub_total
+        Venta_tot.append(Venta)
     except:
         print("Este articulo no existe, intenta con otro")
     finally:
-        Articulo = input("Siguiente articulo o pagar: ")
+        Articulo = str(input("Siguiente articulo o pagar: ").upper())
+
+# finaliza la lista añade el total de la venta e imprime
+Cuenta = ["", "Total", "", "", Total]
+Venta_tot.append(Cuenta)
+Titulos = ["ID", "Articulo", "Unidad", "P.Unit", "Total"]
+print(Titulos)
+for item in Venta_tot:
+    print(item)
