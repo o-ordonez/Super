47c196b366091690e7d95dbad674b5d4dd22b4b9
o-ordonez <omarordonezlima@Air-de-Omar.fritz.box>
2020-06-09 16:47:08 +0200

    Suma de prductos en cons ventas

--- Reporteador.py
+++ Reporteador.py
@@ -1,14 +1,12 @@
 # Abre archivo y añade las ventas realizadas
-Prod = "CACahuate".upper()
-Prodsum = 0
-with open("Cons_ventas.txt") as file:
-    for line in file:
-        s = line.split(",")
-        comparable = str(s[1]).upper()
-        print(comparable)
-        if comparable == Prod:
-            Prodsum += int(s[4])
-        else:
-            print("no es igual")
-            Prodsum += 0
-    print(Prodsum)
+LisdeProd = ["PEPITA", "CACAHUATE"]
+for Prod in LisdeProd:
+    Prodsum = 0
+    with open("Cons_ventas.txt") as file:
+        print(Prod)
+        for line in file:
+            s = line.split(",")
+            comparable = str(s[1]).upper()
+            if comparable == Prod:
+                Prodsum += int(s[4])
+        print(f"El total de {Prod} es: {Prodsum}")
